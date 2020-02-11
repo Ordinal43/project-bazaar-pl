e862f86828764f2051d116568d56667ecc17d609
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2020-02-12 01:45:37 +0700

    Product card column styling

--- resources/js/components/Customer/AppAllProducts.vue
+++ resources/js/components/Customer/AppAllProducts.vue
@@ -20,7 +20,7 @@
         </v-layout>
 
         <v-layout row wrap>
-            <v-flex xs12 sm6 md4 lg3 v-for="(item, id) in getFilteredProducts" :key="`produk-${id}`">
+            <v-flex xs6 md4 lg3 v-for="(item, id) in getFilteredProducts" :key="`produk-${id}`">
                 <product-card :item="item"></product-card>
             </v-flex>
         </v-layout>

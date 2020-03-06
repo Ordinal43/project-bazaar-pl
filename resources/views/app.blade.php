53f5ad49a549bbf09965d0d0ec9e10ba0064c84b
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2020-03-06 14:47:06 +0700

    Added meta fields

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="theme-color" content="#512DA8"/>
 
         <title>Bazaar PL</title>
         <link rel="stylesheet" sizes="192x192" href="{{ mix('/css/app.css') }}">
